0825839e6276d29cacc01d0bb14f63ceac7b38e5
James Gisele <james.may.gisele@protonmail.com>
2021-11-04 18:47:10 -0400

    final soln!

--- app.js
+++ app.js
@@ -30,6 +30,20 @@ let removeButtonsList = {
     }
 };
 
+//when window loads, add all the event handlers and calculate necessary values
+function onLoad() {
+    //when window reloads, make sure all the "checked" inputs remain crossed through
+    todoListItems.checked.forEach(input => {
+        input.parentElement.style.textDecoration = "line-through";
+        input.parentElement.style.color = "hsl(234, 11%, 52%)";
+        itemsLeft();
+    });
+    removeButtons();
+    itemsLeft();
+    crossthroughText();
+}
+window.onload = onLoad();
+
 // When the checkbox is checked, crossthrough text and change its color
 function crossthroughText() {
     todoListItems.allInputs.forEach(input => {
@@ -43,13 +57,13 @@ function crossthroughText() {
             } else {
                 //it's unchecked
                 input.parentElement.style.textDecoration = "none";
-                input.parentElement.style.color = "hsl(234, 39%, 85%)";
+                input.parentElement.style.color = "var(--foreground)";
                 itemsLeft();
             }
         });
     });
 }
-crossthroughText();
+
 
 //When x button is clicked, remove todo 
 function removeButtons() {
@@ -59,14 +73,14 @@ function removeButtons() {
         });
     }
 }
-removeButtons();
+
+
 
 // Display number of todo items unchecked in p #items-left
 function itemsLeft() {
     let itemsLeftText = document.querySelector("#items-left");
     itemsLeftText.innerText = `${todoListItems.unchecked.length} items left`
 }
-itemsLeft();
 
 
 // When input text is entered, append a box to the top of the todo-list-box containing its info
@@ -82,9 +96,14 @@ todoInput.addEventListener("keyup", function (entry) {
     }
 });
 
+let idNumber = todoListItems.all.length;
+
 function newCheckboxTodo(value) {
     const newItem = document.createElement("label");
     newItem.classList.add("checkbox-container");
+    // newItem.setAttribute('draggable', true)
+    idNumber++;
+    newItem.id = `item${idNumber}`
     newItem.innerHTML = `${value}
             <input type="checkbox">
             <span class="checkmark"></span>
@@ -106,11 +125,6 @@ document.querySelector("#clear-completed-button").addEventListener("click", func
 })
 
 
-
-
-
-
-
 // When a view toggle button is pressed, unpress the other two, change color
 const toggleButtons = document.querySelectorAll(".toggle-view-button");
 const allButton = toggleButtons[0];
@@ -125,10 +139,34 @@ for (let i = 0; i < toggleButtons.length; i++) {
 function toggleButton() {
     this.value = "on";
     this.style.color = "hsl(220, 98%, 61%)";
+    this.addEventListener("mouseenter", function () {
+        this.style.color = "hsl(220, 98%, 61%)";
+    });
+    this.addEventListener("mouseout", function () {
+        this.style.color = "hsl(220, 98%, 61%)";
+    });
     for (let i = 0; i < toggleButtons.length; i++) {
         if (toggleButtons[i] !== this) {
             toggleButtons[i].value = "off";
             toggleButtons[i].style.color = "hsl(234, 11%, 52%)";
+            toggleButtons[i].addEventListener("mouseenter", function () {
+                this.style.color = "var(--foreground)";
+            });
+            toggleButtons[i].addEventListener("mouseout", function () {
+                this.style.color = "hsl(234, 11%, 52%)";
+            });
+
+
+
+
+
+
+
+
+
+
+
+
         }
     }
 };
@@ -156,9 +194,24 @@ completedButton.addEventListener("click", function () {
     });
 });
 
-
-
 // When a box is dragged, reorder list
+new Sortable(todoBox, {
+    animation: 150,
+    ghostClass: 'blue-background-class'
+});
 
+//toggle the color scheme
+let toggle = "darkTheme";
+document.querySelector("#color-toggle").addEventListener("click", function () {
+    if (toggle == "darkTheme") {
+        toggle = "lightTheme";
+        document.querySelector("body").classList.add("light-theme");
+        document.querySelector("#color-toggle").src = "images/icon-moon.svg"
+    } else {
+        toggle = "darkTheme";
+        document.querySelector("body").classList.remove("light-theme");
+        document.querySelector("#color-toggle").src = "images/icon-sun.svg"
 
+    }
+})
 
