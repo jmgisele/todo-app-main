a55c9ce076ab194c4ed7d9042de283b67a4c7698
jmgisele <james.may.gisele@protonmail.com>
2022-01-19 19:03:06 -0500

    added IndexedDB storage

--- README.md
+++ README.md
@@ -29,6 +29,7 @@ Users should be able to:
 - Clear all completed todos
 - Toggle light and dark mode
 - **Bonus**: Drag and drop to reorder items on the list
+- **Extra Bonus**: Persistent storage
 
 ### Screenshot
 
@@ -48,18 +49,26 @@ Users should be able to:
 - Mobile-first workflow
 - Sortable.js
 - Semantic and Accessible HTML
-- Node.js
-- Express
-- MongoDB
+- IndexedDB (IDB)
+- [IndexedDB promised library](https://github.com/jakearchibald/idb)
+
 
 ### What I learned
-I decided to come back to this app after a few months and re-make it with persistent storage! I did my best to make the form accessible via keyboard as well, as I was less experienced when I coded this originally and wasn't aware of best practices. Storage is handled client-side using the IDB. This is my first project using client-side storage other than basic cookies/session, so it was a good easy way to test stuff out.
+I decided to come back to this app after a few months and re-make it with persistent storage! I did my best to make the form accessible via keyboard as well, as I was less experienced when I coded this originally and wasn't aware of best practices. Storage is handled client-side using IndexedDB. This is my first project using client-side storage other than basic cookies/session, so it was a good easy way to test stuff out. 
+
+I ended up using the promised-IDB library by Jake Archibald ([link](https://github.com/jakearchibald/idb)) which made things a lot easier for my async/await-trained skills. I figured client-side storage would be simpler than setting up a whole backend. . . but honestly, I'm not that sure. MongoDB/Express and Heroku make things pretty easy to set up, whereas IndexDB is a bit of a pain. The [MDN Guide](https://developer.mozilla.org/en-US/docs/Web/API/IndexedDB_API) was helpful as always, but the syntax feels out of date and unfamiliar. If I were remaking this project, I'd probably just whip up a quick Node/Express/Mongo backend with session that refreshed after a week or so and cleared entries out. That way users wouldn't have to log in but could have semi-persistent storage using session. But this was my first project with client-side storage, it was good for learning a new skill, and I'm just that much more appreciative of async/await functionality now. 
+
+Some bugs I'm aware of but didn't feel like ironing out:
+- Order of the todos doesn't persist on re-load. This could be implemented using a property on the IDB entry for each todo that kept track of its place in the page, maybe using on drop handlers thru sortable.
+-The way I handle keys for IDB vs ids for the DOM is...creative. I'm sure IDB has a better way of keeping track of things using auto-increment but after messing around with IDB for a day I decided I'd rather simply design an id that would assign keys uniquely and be done with it. If this were a production app whose code would be handed down for years I'd find a way to implement it more transparently!
 
 ### Useful resources
 
 - [abhik-b's solution](https://github.com/abhik-b/frontend-challenge-3) - was very helpful in figuring out Sortable.js, which I'd never used before!
 - [Here is SortableJS](https://sortablejs.github.io/Sortable/), which is what I used to create the drag-and-drop functionality.
 - [The MDN Client-side storage guide](https://developer.mozilla.org/en-US/docs/Learn/JavaScript/Client-side_web_APIs/Client-side_storage) conveniently used an example of a todo app.
+- [Jake Archibald's IDB promised library](https://github.com/jakearchibald/idb) saved me a lot of IDB woes and makes interacting with the db more intuitive
+
 
 ## Author
 
--- home.js
+++ home.js
@@ -9,7 +9,7 @@ let todoUncheckedInputs = () => document.querySelectorAll(".checkbox-container i
 //these don't change, no need to recompute
 const todoInput = document.querySelector("#input-text");
 const todoBox = document.querySelector("#todo-list-box");
-const submitForm = document.querySelector("#input-text-form")
+const submitForm = document.querySelector("#input-text-form");
 
 //for local storage
 let db;
@@ -32,19 +32,19 @@ window.onload = () => {
 
 //////////// Callbacks ////////////
 //creating DB
-let createDB = async () => {
+const createDB = async () => {
     const dbName = 'todos_db';
     const version = 1;
 
     db = await idb.openDB(dbName, version, {
         upgrade(db, oldVersion, newVersion, transaction) {
-            const store = db.createObjectStore('todos')
+            const store = db.createObjectStore('todos');
         }
     });
 
     //putting initial values in if none are there on load as placeholder
     const initial_items = await db.transaction('todos').objectStore('todos').getAll();
-    const initial_keys = await db.transaction('todos').objectStore('todos').getAllKeys()
+    const initial_keys = await db.transaction('todos').objectStore('todos').getAllKeys();
     id = initial_keys.length > 0 ? Math.max(...initial_keys, 1) + 1 : 1; //making sure we don't duplicate ids
     if (initial_items.length === 0) { //initial seed
         let initial_todos = [
@@ -56,18 +56,18 @@ let createDB = async () => {
             "Complete Todo app on Front End Mentor"
         ]
         for (let entry of initial_todos) {
-            let transaction = db.transaction('todos', 'readwrite')
-            let store = await transaction.objectStore('todos')
+            let transaction = db.transaction('todos', 'readwrite');
+            let store = await transaction.objectStore('todos');
             let newEntry = { id: id, body: entry, isChecked: false };
-            if (entry === "Complete Todo app on Front End Mentor") newEntry = { id: id, body: entry, isChecked: true };
+            if (entry === "Complete Todo app on Front End Mentor") newEntry = { id: id, body: entry, isChecked: true }; //cute detail
             let placed_val = await store.put(newEntry, id);
-            await transaction.done
-            displayNewItem(newEntry)
+            await transaction.done;
+            displayNewItem(newEntry);
             id++;
         }
     } else { //refresh!
         for (let item of initial_items) {
-            displayNewItem(item)
+            displayNewItem(item);
         }
     }
 }
@@ -83,14 +83,14 @@ let addData = async (e) => {
     const placed_val = await store.put(newItem, id);
     await transaction.done;
     displayNewItem(newItem);
-    e.srcElement[0].value = ""
+    e.srcElement[0].value = "";
     id++;
 }
 
 //displays new todo at top of list
 let displayNewItem = (data) => {
     let value = data.body;
-    let thisID = data.id
+    let thisID = data.id;
     const newItem = document.createElement("label");
     newItem.classList.add("checkbox-container");
     newItem.innerHTML = `${value}
@@ -111,10 +111,10 @@ let displayNewItem = (data) => {
     //if you're currently viewing only completed todos, hide todo
     newItem.style.display = completedButton.classList.contains('toggle-selected') ? "none" : "block";
 
-    // and if it's checked (on a reload), display that 
+    // and if it's checked (only happens on a reload), display that 
     if (data.isChecked) {
         document.querySelector(`#item${thisID}`).checked = true;
-        crossStyling(document.querySelector(`#item${thisID}`))
+        crossStyling(document.querySelector(`#item${thisID}`));
     }
 }
 
@@ -124,14 +124,14 @@ let removeData = async (param) => {
     //if the param is an event (remove button) vs when the param is an input (clear completed button)
     let key = param.target ? parseInt(param.target.id.replace('removebtn', '')) : parseInt(param.id.replace('item', ''));
 
-    const tx = await db.transaction('todos', 'readwrite')
-    const store = await tx.objectStore('todos')
+    const transaction = await db.transaction('todos', 'readwrite');
+    const store = await transaction.objectStore('todos');
 
-    await store.delete(key)
-    await tx.done
+    await store.delete(key);
+    await transaction.done;
 
-    param.target ? param.target.parentElement.remove() : param.parentElement.remove()
-    itemsLeft()
+    param.target ? param.target.parentElement.remove() : param.parentElement.remove();
+    itemsLeft();
 }
 
 
@@ -141,33 +141,32 @@ let removeData = async (param) => {
 // display items left in control box
 let itemsLeft = () => {
     let itemsLeftText = document.querySelector("#items-left");
-    itemsLeftText.innerText = `${todoUncheckedInputs().length} items left`
+    itemsLeftText.innerText = `${todoUncheckedInputs().length} items left`;
 }
 
 // when checkbox is checked, update database to reflect + style
 let crossThrough = async (e) => {
     let key = parseInt(e.target.id.replace('item', ''));
 
-    const tx = await db.transaction('todos', 'readwrite')
-    const store = await tx.objectStore('todos')
+    const transaction = await db.transaction('todos', 'readwrite');
+    const store = await transaction.objectStore('todos');
 
     const thisEntry = await store.get(key);
-    await store.put({ id: key, body: thisEntry.body, isChecked: !thisEntry.isChecked }, key)
-    await tx.done
+    await store.put({ id: key, body: thisEntry.body, isChecked: !thisEntry.isChecked }, key);
+    await transaction.done;
 
     //styling
-    crossStyling(e.target)
+    crossStyling(e.target);
 }
 
 
 let crossStyling = (input) => {
     if (input.checked) {
-        input.parentElement.classList.add("crossed-out")
-        input.parentElement.classList.remove("not-crossed")
+        input.parentElement.classList.add("crossed-out");
+        input.parentElement.classList.remove("not-crossed");
     } else { //it's unchecked
-        input.parentElement.classList.add("not-crossed")
-        input.parentElement.classList.remove("crossed-out")
-
+        input.parentElement.classList.add("not-crossed");
+        input.parentElement.classList.remove("crossed-out");
     }
     itemsLeft();
 }
@@ -177,7 +176,7 @@ let crossStyling = (input) => {
 
 // when clear button is pressed, clear completed
 document.querySelector("#clear-completed-button").addEventListener("click", () => {
-    todoCheckedInputs().forEach(input =>  removeData(input))
+    todoCheckedInputs().forEach(input =>  removeData(input));
     itemsLeft(); //shouldn't change - fallback
 })
 
@@ -191,18 +190,18 @@ const completedButton = toggleButtons[2];
 //toggles the correct styles
 function toggleButtonStyles(e) {
     let target = e.target;
-    target.classList.add("toggle-selected")
-    target.classList.remove("toggle-unselected")
+    target.classList.add("toggle-selected");
+    target.classList.remove("toggle-unselected");
     Array.from(toggleButtons).filter((btn) => btn !== target)
         .forEach((btn) => {
-            btn.classList.remove("toggle-selected")
-            btn.classList.add("toggle-unselected")
+            btn.classList.remove("toggle-selected");
+            btn.classList.add("toggle-unselected");
         })
 };
 
 // changing views on toggle button click
 allButton.addEventListener("click", (e) => {
-    toggleButtonStyles(e)
+    toggleButtonStyles(e);
     todoAllInputs().forEach(input => {
         input.parentElement.style.display = "block";
     });
@@ -236,12 +235,10 @@ document.querySelector("#color-toggle").addEventListener("click", () => {
     if (toggle == "darkTheme") {
         toggle = "lightTheme";
         document.querySelector("body").classList.add("light-theme");
-        document.querySelector("#color-toggle").src = "images/icon-moon.svg"
+        document.querySelector("#color-toggle").src = "images/icon-moon.svg";
     } else {
         toggle = "darkTheme";
         document.querySelector("body").classList.remove("light-theme");
-        document.querySelector("#color-toggle").src = "images/icon-sun.svg"
-
+        document.querySelector("#color-toggle").src = "images/icon-sun.svg";
     }
 })
-
